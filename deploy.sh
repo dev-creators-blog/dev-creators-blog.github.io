e65b9f481bd8a2aac0ddedcd1cc4dd992bac7933
nana07 <xxstigmatizexx@gmail.com>
2015-09-03 12:32:51 +0900

    ブランチの設定を変更

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 branch=`git rev-parse --abbrev-ref @`
-if [ "$branch" = "master" ]; then
+if [ "$branch" = "source" ]; then
   echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
   # Build the project.
@@ -18,11 +18,11 @@ if [ "$branch" = "master" ]; then
   git commit -m "$msg"
 
   # Push source and build repos.
-  git push origin master 
-  git subtree push --prefix=public origin gh-pages 
+  git push origin source
+  git subtree push --prefix=public origin master
 
 else
-  echo -e "\033[0;31merror: Current branch is not a master.\033[0m" 1>&2
+  echo -e "\033[0;31merror: Current branch is not a source.\033[0m" 1>&2
   exit 1
 fi
 
